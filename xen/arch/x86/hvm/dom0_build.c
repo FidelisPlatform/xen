3512ffd05ff2daa324e42819fedfde8532ca913e
Stefano Stabellini <stefano.stabellini@amd.com>
2023-05-25 16:18:51 -0700

    xen/x86/pvh: handle ACPI RSDT table in PVH Dom0 build
    Xen always generates a XSDT table even if the firmware provided a RSDT
    table. Copy the RSDT header from the firmware table when the XSDT table
    is missing.
    
    Fixes: 1d74282c455f ('x86: setup PVHv2 Dom0 ACPI tables')
    Suggested-by: Roger Pau Monné <roger.pau@citrix.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/x86/hvm/dom0_build.c
+++ xen/arch/x86/hvm/dom0_build.c
@@ -970,7 +970,16 @@ static int __init pvh_setup_acpi_xsdt(struct domain *d, paddr_t madt_addr,
         rc = -EINVAL;
         goto out;
     }
-    xsdt_paddr = rsdp->xsdt_physical_address;
+    /*
+     * Note the header is the same for both RSDT and XSDT, so it's fine to
+     * copy the native RSDT header to the Xen crafted XSDT if no native
+     * XSDT is available.
+     */
+    if ( rsdp->revision > 1 && rsdp->xsdt_physical_address )
+        xsdt_paddr = rsdp->xsdt_physical_address;
+    else
+        xsdt_paddr = rsdp->rsdt_physical_address;
+
     acpi_os_unmap_memory(rsdp, sizeof(*rsdp));
     table = acpi_os_map_memory(xsdt_paddr, sizeof(*table));
     if ( !table )
